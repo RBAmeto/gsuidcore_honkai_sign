22bd6da13b1d1aff2ee615cc147197a41fb04d50
RBAmeto <liurbang@gmail.com>
2023-04-11 09:48:49 +0800

    first commit

---
+++ __init__.py
@@ -0,0 +1,214 @@
+import asyncio
+import json
+import os
+from datetime import datetime
+# import honkai3rd
+# import setting
+from . import until
+
+from gsuid_core.sv import SV
+from gsuid_core.bot import Bot
+from gsuid_core.models import Event
+from ..utils.database import get_sqla
+from ..utils.error_reply import UID_HINT
+from gsuid_core.gss import gss
+from gsuid_core.logger import logger
+from gsuid_core.aps import scheduler
+
+sv_hk_sign = SV("崩坏3米游社签到")
+sv_hk_sign_config = SV("崩坏3米游社签到", pm=1)
+# _bot = sv.bot
+
+
+# async def autosign(hk3: Honkai3rd, qid: str):
+#     sign_data = load_data()
+#     today = datetime.today().day
+#     try:
+#         result_list = await hk3.sign_account()
+#         print(result_list)
+#     except Exception as e:
+#         sign_data.update({qid: {"date": today, "status": False, "result": None}})
+#         return f"{e}\n自动签到失败."
+#     ret_list = ""
+#     ret_list += result_list
+#     print(ret_list)
+#     sign_data.update({qid: {"date": today, "status": True, "result": ret_list}})
+#     save_data(sign_data)
+#     return ret_list
+
+
+SIGN_PATH = os.path.join(os.path.dirname(__file__), "./sign_on.json")
+
+
+def load_data():
+    if not os.path.exists(SIGN_PATH):
+        with open(SIGN_PATH, "w", encoding="utf8") as f:
+            json.dump({}, f)
+            return {}
+    with open(SIGN_PATH, "r", encoding="utf8") as f:
+        data: dict = json.load(f)
+        return data
+
+
+def save_data(data):
+    with open(SIGN_PATH, "w", encoding="utf8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+# async def check_cookie(qid: str):
+#     uid = await select_db(qid, mode='uid')
+#     cookie = await owner_cookies(uid)
+#     if not cookie:
+#         return f"自动签到需要绑定cookie,发送'bhf?'查看如何绑定."
+#     hk3 = Honkai3rd(cookie=cookie)
+#     try:
+#         role_info = hk3.roles_info
+#     except GenshinHelperException as e:
+#         return f"{e}\ncookie不可用,请重新绑定."
+#     if not role_info:
+#         return f"未找到崩坏3角色信息,请确认cookie对应账号是否已绑定崩坏3角色."
+#     return hk3
+
+
+# @sv.on_prefix(r"bh(开启|关闭|on|off)?\s?自动签到")
+@sv_hk_sign.on_prefix(('bh开启','bh关闭'))
+async def switch_autosign(bot: Bot, ev: Event):
+    """自动签到开关"""
+    today = datetime.today().day
+    qid = str(ev.user_id)
+    gid = str(ev.group_id)
+    config_name = ev.text
+    sign_data = load_data()
+    if not config_name =='自动签到':
+        return
+    if ev.command == 'bh关闭':
+        if not qid in sign_data:
+            return
+        sign_data.pop(qid)
+        save_data(sign_data)
+        await bot.send("崩3签到已关闭.", at_sender=True)
+        return
+    flag = False
+    try:
+        result,flag = await until.sign_bh3(qid)
+    except Exception as e: 
+        print(e)
+    if flag:
+        today = datetime.today().day
+        sign_data.update({qid: {"gid": gid, "date": today, "status": True, "result": result}})
+        save_data(sign_data)
+        await bot.send(result)
+    else:
+        await bot.send(f"签到失败", at_sender=True)
+
+
+
+
+
+
+async def send_notice(qid: str,gid: str, context: str, bot: Bot):
+    try:
+        for bot_id in gss.active_bot:
+            await gss.active_bot[bot_id].target_send(
+                    context,
+                    'group',
+                    gid,
+                    bot_id,
+                    '',
+                    '',
+                )
+    except Exception as e:
+            logger.warning(f'[崩3签到]群 {gid} 推送失败!错误信息:{e}')
+    return
+
+
+@scheduler.scheduled_job("cron", hour="0", minute="10,40")
+async def schedule_sign():
+    today = datetime.today().day
+    sign_data = load_data()
+    cnt = 0
+    sum = len(sign_data)
+    for qid in sign_data:
+        await asyncio.sleep(5)
+        if sign_data[qid].get("date") != today or not sign_data[qid].get("status"):
+            flag = False
+            try:
+                result,flag = await until.sign_bh3(qid)
+            except Exception as e: 
+                print(e)
+            gid = sign_data[qid].get("gid")
+            if flag:
+                today = datetime.today().day
+                sign_data.update({qid: {"gid": gid, "date": today, "status": True, "result": result}})
+                save_data(sign_data)
+                await send_notice(qid,gid, result)
+            else:
+                await send_notice(qid,gid, f"[CQ:at,qq={qid}] 签到失败")
+    return cnt, sum
+
+
+@sv_hk_sign_config.on_fullmatch("崩3全部重签")
+async def reload_sign(bot: Bot, ev: Event):
+    await bot.send("开始重执行。")
+    try:
+        cnt, sum = await schedule_sign()
+    except:
+        res = await schedule_sign()
+    await bot.send(
+        ev,
+        f"重执行完成，状态刷新{cnt}条，共{sum}条",
+        at_sender=True,
+    )
+
+# @sv.on_prefix("删除uid")
+# async def delete_uid(bot: HoshinoBot, ev: CQEvent):
+#     if not priv.check_priv(ev, priv.SUPERUSER):
+#         return
+#     qid = str(ev.message.extract_plain_text()).strip()
+#     if not qid.isnumeric():
+#         await bot.send(ev, "参数错误")
+#         return
+#     im = await delete_cookies(qid)
+#     await bot.send(ev, im)
+
+
+# @sv.on_prefix("gs拉黑")
+# async def delete_uid(bot: HoshinoBot, ev: CQEvent):
+#     if not priv.check_priv(ev, priv.SUPERUSER):
+#         return
+#     qid = str(ev.message.extract_plain_text()).strip()
+#     if not qid.isnumeric():
+#         await bot.send(ev, "参数错误")
+#         return
+#     user_id = int(qid)
+#     uid_list = await select_db(user_id,'list')
+#     im=""
+#     if len(uid_list) >= 1:
+#         for i in uid_list:
+#             im += await delete_db(user_id, {'UID': i})
+#             im += await delete_cookies(i)
+#             im += await set_config_func(
+#                 config_name="自动签到",
+#                 uid=i,  # type: ignore
+#                 qid=qid,  # type: ignore
+#                 option='off',
+#                 query = 'CLOSED',
+#                 is_admin=True,
+#             )
+#             im += await set_config_func(
+#                 config_name="推送",
+#                 uid=i,  # type: ignore
+#                 qid=qid,  # type: ignore
+#                 option='off',
+#                 query = 'CLOSED',
+#                 is_admin=True,
+#             )
+#             im += await set_config_func(
+#                 config_name="自动米游币",
+#                 uid=i,  # type: ignore
+#                 qid=qid,  # type: ignore
+#                 option='off',
+#                 query = 'CLOSED',
+#                 is_admin=True,
+#             )
+#     await bot.send(ev, im)
---
+++ until.py
@@ -0,0 +1,172 @@
+import copy
+import random
+import asyncio
+import re
+# from ..utils.db_operation.db_operation import owner_cookies,select_db
+# # from ..utils.db_operation.database.models import CK
+# from ..utils.mhy_api.mhy_api_tools import generate_dynamic_secret,random_hex,old_version_get_ds_token
+# from ..utils.mhy_api.get_mhy_data import(
+#     _HEADER,
+#     mys_api._mys_request,
+#     _pass
+# )
+
+from gsuid_core.gss import gss
+from ..gsuid_utils.api.mys.request import _HEADER
+from ..gsuid_utils.api.mys.tools import random_hex,get_web_ds_token
+from ..utils.mys_api import mys_api
+from ..utils.database import get_sqla
+web_api = "https://api-takumi.mihoyo.com"
+honkai3rd_Act_id = "e202207181446311"
+honkai3rd_checkin_rewards = f'{web_api}/event/luna/home?lang=zh-cn&act_id={honkai3rd_Act_id}'
+honkai3rd_Is_signurl = web_api + "/event/luna/info?lang=zh-cn&act_id={}&region={}&uid={}"
+honkai3rd_Sign_url = web_api + "/event/luna/sign"
+account_Info_url = web_api + "/binding/api/getUserGameRolesByCookie?game_biz="
+
+
+
+
+async def get_account_list(cookie,game_id= "bh3_cn") -> list:
+    print(f"正在获取米哈游账号绑定的{game_id}账号列表...")
+    HEADER = copy.deepcopy(_HEADER)
+    HEADER['Cookie'] = cookie
+    HEADER['DS'] = get_web_ds_token(True)
+    temp_list = []
+    data = await mys_api._mys_request(
+            url=account_Info_url+ game_id,
+            method='GET',
+            header=HEADER
+        )
+    if data["retcode"] != 0:
+        print(f"获取{game_id}账号列表失败！")
+    for i in data["data"]["list"]:
+        temp_list.append([i["nickname"], i["game_uid"], i["region"]])
+    print(f"已获取到{len(temp_list)}个{game_id}账号信息")
+    return temp_list
+
+
+
+async def sign_bh3(qid) -> str:
+    for bot_id in gss.active_bot:
+        sqla = get_sqla(bot_id)
+        uid = await sqla.get_bind_uid(qid)
+        return_data = f"[CQ:at,qq={qid}] "
+        if uid is None:
+            return return_data + "你没有绑定过原神uid嗷~",False
+        cookie = await mys_api.get_ck(uid, 'OWNER')
+        if cookie is None:
+            return return_data + "你没有绑定过Cookies噢~",False
+        account_list = await get_account_list(cookie)
+        if len(account_list) == 0:
+            return return_data + "未获取到崩坏3账号信息",False
+        checkin_rewards = await get_checkin_rewards()
+        for i in account_list:
+            match = "^玩家[0-9]{8,9}"
+            if re.match(match, i[0]):
+                continue
+            is_data = await is_sign(region = i[2], uid = i[1], cookie = cookie)
+            if is_data["is_sign"]:
+                getitem = checkin_rewards[int(is_data['total_sign_day']) - 1]
+                return_data += f"舰长:{i[0]}今天已经签到过了~\r\n今天获得的奖励是{getitem['name']}x{getitem['cnt']}\n"
+            elif is_data["is_sign"] == False:
+                Header = {}
+                for index in range(4):
+                    # 进行一次签到
+                    sign_data = await sign(uid=i[1], server_id = i[2], cookie = cookie, Header=Header)
+                    print(sign_data)
+                    # 检测数据
+                    if sign_data and 'data' in sign_data and sign_data['data']:
+                        if 'risk_code' in sign_data['data']:
+                            if sign_data['data']['risk_code'] == 375 or sign_data['data']['risk_code'] == 5001:
+                            # 出现校验码                       
+                                gt = sign_data['data']['gt']
+                                ch = sign_data['data']['challenge']
+                                vl, ch = await mys_api._pass(gt, ch, Header)
+                                if vl:
+                                    delay = 1
+                                    Header['x-rpc-challenge'] = ch
+                                    Header['x-rpc-validate'] = vl
+                                    Header['x-rpc-seccode'] = f'{vl}|jordan'
+                                    print(f'[签到] {i[0]} 已获取验证码, 等待时间{delay}秒')
+                                    await asyncio.sleep(delay)
+                                else:
+                                    delay = 300 + random.randint(1, 120)
+                                    print(f'[签到] {i[0]} 未获取验证码,等待{delay}秒后重试...')
+                                    await asyncio.sleep(delay)
+                                continue
+                            else:
+                                if index == 0:
+                                    print(f'[签到] {i[0]} 该用户无校验码!')
+                                else:
+                                    print(f'[签到] [无感验证] {i[0]} 该用户重试 {index} 次验证成功!')
+                                getitem = checkin_rewards[int(is_data['total_sign_day']) - 1 + 1]
+                                return_data += f"舰长:{i[0]}签到成功~\r\n今天获得的奖励是{getitem['name']}x{getitem['cnt']}\n"
+                                break
+                        else:
+                            # 重试超过阈值
+                            print('[签到] 超过请求阈值...')
+                            return_data += f"舰长:{i[0]}签到失败~出现验证码\r\n{sign_data.text}\n"
+                # 签到失败
+                else:
+                    return_data += f"舰长:{i[0]}签到失败~"
+            else:
+                return_data += f"舰长:{i[0]}签到失败\r\n{is_data.text}\n"
+        return return_data,True
+
+
+async def get_checkin_rewards():
+    print("正在获取签到奖励列表...")
+    data = await mys_api._mys_request(
+            url=honkai3rd_checkin_rewards,
+            method='GET',
+            header=_HEADER
+            )
+    if data["retcode"] != 0:
+        print("获取签到奖励列表失败")
+        print(data.text)
+    return data["data"]["awards"]
+
+async def is_sign(region: str, uid: str,cookie) -> dict:
+    url = honkai3rd_Is_signurl.format(honkai3rd_Act_id, region, uid)
+    # print(url)
+    HEADER = copy.deepcopy(_HEADER)
+    HEADER['Cookie'] = cookie
+    data = await mys_api._mys_request(
+            url=url,
+            method='GET',
+            header=HEADER
+            )
+    # print(data)
+    if data["retcode"] != 0:
+        print("获取账号签到信息失败！")
+        print(data)
+    return data["data"]
+
+async def sign(uid,server_id = "pc01", cookie = None ,Header={}):
+    HEADER = copy.deepcopy(_HEADER)
+    HEADER['User_Agent'] = (
+        'Mozilla/5.0 (Linux; Android 10; MIX 2 Build/QKQ1.190825.002; wv) '
+        'AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 '
+        'Chrome/83.0.4103.101 Mobile Safari/537.36 miHoYoBBS/2.44.1'
+    )
+    HEADER['Cookie'] = cookie
+    HEADER['x-rpc-device_id'] = random_hex(32)
+    HEADER['x-rpc-app_version'] = '2.44.1'
+    HEADER['x-rpc-client_type'] = '5'
+    HEADER['X_Requested_With'] = 'com.mihoyo.hyperion'
+    HEADER['DS'] = get_web_ds_token(True)
+    HEADER['Referer'] = ('https://webstatic.mihoyo.com/bbs/event/signin/bh3/index.html?bbs_auth_required' \
+                        f'=true&act_id={honkai3rd_Act_id}&bbs_presentation_style=fullscreen' \
+                        '&utm_source=bbs&utm_medium=mys&utm_campaign=icon')
+    HEADER.update(Header)
+    data = await mys_api._mys_request(
+        url=honkai3rd_Sign_url,
+        method='POST',
+        header=HEADER,
+        data={
+            'act_id': honkai3rd_Act_id,
+            'uid': uid,
+            'region': server_id,
+        },
+    )
+    return data
