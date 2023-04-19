48ead96a44654c9ae2dab221bd53699c88a0268a
RBAmeto <46624927+RBAmeto@users.noreply.github.com>
2023-04-20 00:52:36 +0800

    fix bot_id bug

--- honkai_sign/until.py
+++ honkai_sign/until.py
@@ -38,75 +38,76 @@ async def get_account_list(cookie,game_id= "bh3_cn") -> list:
 
 
 
-async def sign_bh3(qid) -> str:
-    for bot_id in gss.active_bot:
-        sqla = get_sqla(bot_id)
-        uid = await sqla.get_bind_uid(qid)
-        return_data = f"[CQ:at,qq={qid}] "
-        flag = False
-        if uid is None:
-            return return_data + "你没有绑定过原神uid嗷~",flag
-        cookie = await mys_api.get_ck(uid, 'OWNER')
-        if cookie is None:
-            return return_data + "你没有绑定过Cookies噢~",flag
-        account_list = await get_account_list(cookie)
-        # print(account_list)
-        if len(account_list) == 0:
-            return return_data + "未获取到崩坏3账号信息",flag
-        checkin_rewards = await get_checkin_rewards()
-        for i in account_list:
-            match = "^玩家[0-9]{8,9}"
-            if re.match(match, i[0]):
-                continue
-            is_data = await is_sign(region = i[2], uid = i[1], cookie = cookie)
-            if is_data["is_sign"]:
-                getitem = checkin_rewards[int(is_data['total_sign_day']) - 1]
-                return_data += f"舰长:{i[0]}今天已经签到过了~\r\n今天获得的奖励是{getitem['name']}x{getitem['cnt']}\n"
-            elif is_data["is_sign"] == False:
-                Header = {}
-                for index in range(4):
-                    # 进行一次签到
-                    sign_data = await sign(uid=i[1], server_id = i[2], cookie = cookie, Header=Header)
-                    # print(sign_data)
-                    # 检测数据
-                    if sign_data and 'data' in sign_data and sign_data['data']:
-                        if 'risk_code' in sign_data['data']:
-                            if sign_data['data']['risk_code'] == 375 or sign_data['data']['risk_code'] == 5001:
-                            # 出现校验码                       
-                                gt = sign_data['data']['gt']
-                                ch = sign_data['data']['challenge']
-                                vl, ch = await mys_api._pass(gt, ch, Header)
-                                if vl:
-                                    delay = 1
-                                    Header['x-rpc-challenge'] = ch
-                                    Header['x-rpc-validate'] = vl
-                                    Header['x-rpc-seccode'] = f'{vl}|jordan'
-                                    print(f'[签到] {i[0]} 已获取验证码, 等待时间{delay}秒')
-                                    await asyncio.sleep(delay)
-                                else:
-                                    delay = 300 + random.randint(1, 120)
-                                    print(f'[签到] {i[0]} 未获取验证码,等待{delay}秒后重试...')
-                                    await asyncio.sleep(delay)
-                                continue
+async def sign_bh3(qid,bot_id = "onebot"):
+    sqla = get_sqla(bot_id)
+    uid = await sqla.get_bind_uid(qid)
+    return_data = f"[CQ:at,qq={qid}] "
+    flag = False
+    if uid is None:
+        return return_data + "你没有绑定过原神uid嗷~",flag
+    cookie = await mys_api.get_ck(uid, 'OWNER')
+    if cookie is None:
+        return return_data + "你没有绑定过Cookies噢~",flag
+    account_list = await get_account_list(cookie)
+    # print(account_list)
+    if len(account_list) == 0:
+        return return_data + "未获取到崩坏3账号信息",flag
+    checkin_rewards = await get_checkin_rewards()
+    for i in account_list:
+        match = "^玩家[0-9]{8,9}"
+        if re.match(match, i[0]):
+            continue
+        is_data = await is_sign(region = i[2], uid = i[1], cookie = cookie)
+        if is_data["is_sign"]:
+            getitem = checkin_rewards[int(is_data['total_sign_day']) - 1]
+            return_data += f"舰长:{i[0]}今天已经签到过了~\r\n今天获得的奖励是{getitem['name']}x{getitem['cnt']}\n"
+            flag = True
+            return return_data,flag
+        elif is_data["is_sign"] == False:
+            Header = {}
+            for index in range(4):
+                # 进行一次签到
+                sign_data = await sign(uid=i[1], server_id = i[2], cookie = cookie, Header=Header)
+                # print(sign_data)
+                # 检测数据
+                if sign_data and 'data' in sign_data and sign_data['data']:
+                    if 'risk_code' in sign_data['data']:
+                        if sign_data['data']['risk_code'] == 375 or sign_data['data']['risk_code'] == 5001:
+                        # 出现校验码                       
+                            gt = sign_data['data']['gt']
+                            ch = sign_data['data']['challenge']
+                            vl, ch = await mys_api._pass(gt, ch, Header)
+                            if vl:
+                                delay = 1
+                                Header['x-rpc-challenge'] = ch
+                                Header['x-rpc-validate'] = vl
+                                Header['x-rpc-seccode'] = f'{vl}|jordan'
+                                print(f'[签到] {i[0]} 已获取验证码, 等待时间{delay}秒')
+                                await asyncio.sleep(delay)
                             else:
-                                if index == 0:
-                                    print(f'[签到] {i[0]} 该用户无校验码!')
-                                else:
-                                    print(f'[签到] [无感验证] {i[0]} 该用户重试 {index} 次验证成功!')
-                                flag = True
-                                getitem = checkin_rewards[int(is_data['total_sign_day']) - 1 + 1]
-                                return_data += f"舰长:{i[0]}签到成功~\r\n今天获得的奖励是{getitem['name']}x{getitem['cnt']}\n"
-                                break
+                                delay = 300 + random.randint(1, 120)
+                                print(f'[签到] {i[0]} 未获取验证码,等待{delay}秒后重试...')
+                                await asyncio.sleep(delay)
+                            continue
                         else:
-                            # 重试超过阈值
-                            print('[签到] 超过请求阈值...')
-                            return_data += f"舰长:{i[0]}签到失败~出现验证码\r\n{sign_data['data'].text}\n"
-                    # 签到失败
-                else:
-                    return_data += f"舰长:{i[0]}签到失败~"
+                            if index == 0:
+                                print(f'[签到] {i[0]} 该用户无校验码!')
+                            else:
+                                print(f'[签到] [无感验证] {i[0]} 该用户重试 {index} 次验证成功!')
+                            flag = True
+                            getitem = checkin_rewards[int(is_data['total_sign_day']) - 1 + 1]
+                            return_data += f"舰长:{i[0]}签到成功~\r\n今天获得的奖励是{getitem['name']}x{getitem['cnt']}\n"
+                            break
+                    else:
+                        # 重试超过阈值
+                        print('[签到] 超过请求阈值...')
+                        return_data += f"舰长:{i[0]}签到失败~出现验证码\r\n{sign_data['data'].text}\n"
+                # 签到失败
             else:
-                return_data += f"舰长:{i[0]}签到失败\r\n{is_data.text}\n"
-        return return_data,flag
+                return_data += f"舰长:{i[0]}签到失败~"
+        else:
+            return_data += f"舰长:{i[0]}签到失败\r\n{is_data.text}\n"
+    return return_data,flag
 
 
 async def get_checkin_rewards():
