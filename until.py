92437fe9aa48527c36aa28d665bf33c6eae3cc7d
RBAmeto <46624927+RBAmeto@users.noreply.github.com>
2023-10-17 05:51:54 +0800

    补充报错信息

--- until.py
+++ until.py
@@ -18,46 +18,51 @@ class _MysApi(MysApi):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+
 mys_api = _MysApi()
 
 
-
 async def get_account_list(cookie,game_id= "bh3_cn") -> list:
-    print(f"正在获取米哈游账号绑定的{game_id}账号列表...")
+    print(f"正在获取米哈游账号绑定的崩坏3账号列表...")
     HEADER = copy.deepcopy(mys_api._HEADER)
     HEADER['Cookie'] = cookie
     HEADER['DS'] = get_web_ds_token(True)
-    temp_list = []
     data = await mys_api._mys_request(
             url=account_Info_url+ game_id,
             method='GET',
             header=HEADER
         )
-    if data["retcode"] != 0:
-        print(f"获取{game_id}账号列表失败！")
-    for i in data["data"]["list"]:
-        temp_list.append([i["nickname"], i["game_uid"], i["region"]])
-    print(f"已获取到{len(temp_list)}个{game_id}账号信息")
-    return temp_list
-
+    return data
 
 
 async def sign_bh3(qid,bot_id = "onebot"):
     return_data = f"[CQ:at,qq={qid}] "
     flag = False
     cookie = await GsUser.get_user_cookie_by_user_id(qid,bot_id)
-    if cookie is None:
+    if not cookie:
         return return_data + "你没有绑定过Cookies噢~",flag
-    account_list = await get_account_list(cookie)
-    # print(account_list)
+    account_list = []
+    account_data = await get_account_list(cookie)
+    if isinstance(account_data, int):
+        return return_data + f"获取账号列表失败！",flag
+    for i in account_data["data"]["list"]:
+        account_list.append([i["nickname"], i["game_uid"], i["region"]])
+    print(f"已获取到{len(account_list)}个崩坏3账号信息")
     if len(account_list) == 0:
         return return_data + "未获取到崩坏3账号信息",flag
     checkin_rewards = await get_checkin_rewards()
+    if isinstance(checkin_rewards, int):
+        return return_data + "获取签到奖励列表失败",flag
+    else:
+        checkin_rewards = checkin_rewards["data"]["awards"]
     for i in account_list:
         match = "^玩家[0-9]{8,9}"
         if re.match(match, i[0]):
             continue
         is_data = await is_sign(region = i[2], uid = i[1], cookie = cookie)
+        if isinstance(is_data, int):
+            return return_data + "获取账号签到信息失败！",flag
+        is_data = is_data["data"]
         if is_data["is_sign"]:
             getitem = checkin_rewards[int(is_data['total_sign_day']) - 1]
             return_data += f"舰长:{i[0]}今天已经签到过了~\r\n今天获得的奖励是{getitem['name']}x{getitem['cnt']}\n"
@@ -70,9 +75,16 @@ async def sign_bh3(qid,bot_id = "onebot"):
                 sign_data = await sign(uid=i[1], server_id = i[2], cookie = cookie, Header=Header)
                 # print(sign_data)
                 # 检测数据
-                if sign_data and 'data' in sign_data and sign_data['data']:
+                if isinstance(sign_data, int):
+                    if sign_data == -500001:
+                        delay = 60 + random.randint(1, 30)
+                        await asyncio.sleep(delay)
+                        continue
+                    else:
+                        return return_data + f"舰长:{i[0]}签到失败~错误码:{sign_data}",flag
+                if sign_data and 'data' in sign_data and sign_data['data']:              
                     if 'risk_code' in sign_data['data']:
-                        if sign_data['data']['risk_code'] == 375 or sign_data['data']['risk_code'] == 5001:
+                        if sign_data['data']['risk_code'] == 5001:
                         # 出现校验码                       
                             gt = sign_data['data']['gt']
                             ch = sign_data['data']['challenge']
@@ -106,7 +118,7 @@ async def sign_bh3(qid,bot_id = "onebot"):
             else:
                 return_data += f"舰长:{i[0]}签到失败~"
         else:
-            return_data += f"舰长:{i[0]}签到失败\r\n{is_data.text}\n"
+            return_data += f"舰长:{i[0]}签到失败"
     return return_data,flag
 
 
@@ -117,10 +129,7 @@ async def get_checkin_rewards():
             method='GET',
             header=mys_api._HEADER
             )
-    if data["retcode"] != 0:
-        print("获取签到奖励列表失败")
-        print(data.text)
-    return data["data"]["awards"]
+    return data
 
 async def is_sign(region: str, uid: str,cookie) -> dict:
     url = honkai3rd_Is_signurl.format(honkai3rd_Act_id, region, uid)
@@ -131,11 +140,7 @@ async def is_sign(region: str, uid: str,cookie) -> dict:
             method='GET',
             header=HEADER
             )
-    # print(data)
-    if data["retcode"] != 0:
-        print("获取账号签到信息失败！")
-        print(data)
-    return data["data"]
+    return data
 
 async def sign(uid,server_id = "pc01", cookie = None ,Header={}):
     HEADER = copy.deepcopy(mys_api._HEADER)
